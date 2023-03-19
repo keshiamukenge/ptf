8cd4f7f7c2b3ebc32b9bbad2c0e91ca962c1ae4e
Keshia Mukenge <keshia@MBP-de-Keshia-002.lan>
2023-03-19 15:07:35 +0100

    feat: add footer, fix webgl effect & add texts animations

---
+++ src/js/TextsAnimations.js
@@ -0,0 +1,64 @@
+import SplitInLines from 'lines-split';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+export default class TextsAnimations {
+  constructor() {
+    gsap.registerPlugin(ScrollTrigger);
+
+    this.addStyle();
+    this.upProjectText();
+    this.upProjectInfos();
+  }
+
+  async addStyle() {
+    try {
+      await new SplitInLines('#hero p');
+
+      this.heroTextLines = document.querySelectorAll('#hero p > span > span');
+      this.heroTextLines.forEach((line) => {
+        line.style.transform = 'translateY(100%)';
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  upProjectText() {
+    this.projectsTitles = document.querySelectorAll('#container-section-project .container-title');
+    this.projectsTitles.forEach((title) => {
+      title.style.transform = 'translateY(35%)';
+      title.style.opacity = 0;
+
+      gsap.to(title, {
+        scrollTrigger: {
+          trigger: title,
+          start: 'top 80%',
+        },
+        duration: 0.5,
+        ease: 'power1.inout',
+        opacity: 1,
+        y: 0,
+      });
+    });
+  }
+
+  upProjectInfos() {
+    this.projectInfos = document.querySelectorAll('#container-section-project .projects-section__item-infos');
+    this.projectInfos.forEach((projectInfo, id) => {
+      projectInfo.style.transform = 'translateY(20%)';
+      projectInfo.style.opacity = 0;
+
+      gsap.to(projectInfo, {
+        scrollTrigger: {
+          trigger: this.projectsTitles[id],
+          start: 'top 80%',
+        },
+        duration: 0.5,
+        ease: 'power1.inout',
+        opacity: 1,
+        y: 0,
+      });
+    });
+  }
+}
